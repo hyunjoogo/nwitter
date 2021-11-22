ec820fcfe3a71bab85717240642ec22eac559b48
HyunJoo Kim <hyunjoogo@gmail.com>
2021-11-22 14:54:49 +0900

    setting firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FirebaseApp from "./firebase";
 
+
+console.log(FirebaseApp)
 ReactDOM.render(
   <React.StrictMode>
     <App />
